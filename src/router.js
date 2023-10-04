dd6c0479ab83f2ca389a2326251925613a71abe8
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-10-04 16:38:25 +0300

    accesoryController
    add accesoryController with create view rendering

--- src/router.js
+++ src/router.js
@@ -1,13 +1,18 @@
 const router= require("express").Router();
 
+const accesorieController=require("./controllers/accesoryController")
 const homeController=require("./controllers/homeController")
 const cubeController=require("./controllers/cubeController")
 router.use(homeController)
 router.use("/cubes",cubeController)
+router.use("/accessories",accesorieController)
 router.get("*",(req,res)=>{
 res.redirect("/404")
 
 
+})
+router.use("accesories/create",(req,res)=>{
+res.render("createAccesory")
 })
 
 module.exports=router
