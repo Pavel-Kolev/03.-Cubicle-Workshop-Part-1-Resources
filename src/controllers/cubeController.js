5ec9caeb13069ab27091a3484041629485a075d0
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-10-04 19:09:51 +0300

    cube model rel
    add relation to cube model

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,7 +1,8 @@
 const router = require("express").Router()
 const cubeService = require("../services/cubeService")
+const accessoryService=require('../services/accessoryService')
 router.get('/create', (req, res) => {
-  
+
    res.render("cube/create")
 })
 router.post("/create", async (req, res) => {
@@ -10,15 +11,25 @@ router.post("/create", async (req, res) => {
    await cubeService.create({ name, description, imageUrl, difficultyLevel: Number(difficultyLevel) })
    res.redirect("/")
 })
-router.get('/:cubeId/details',async (req, res) => {
+router.get('/:cubeId/details', async (req, res) => {
    const { cubeId } = req.params;
    const cube = await cubeService.getSingleCube(cubeId).lean()
-   if(!cube){
+   if (!cube) {
       res.redirect("404")
       return
    }
    res.render('cube/details', { cube })
 
 
+})
+
+router.get('/:cubeId/attach-accessory', async (req, res) => {
+   const { cubeId } = req.params
+   const cube = await cubeService.getSingleCube(cubeId).lean()
+   
+   const accesory =await accessoryService.getAll().lean()
+   console.log(accesory)
+   const hasAccessories= accesory.length>0
+   res.render("accesory/attach", { cube,accesory })
 })
 module.exports = router
--- src/services/accessoryService.js
+++ src/services/accessoryService.js
@@ -6,3 +6,4 @@ const acc=require("../models/Accesory")
 
 
 exports.create=(accessoryData)=>acc.create(accessoryData)
+exports.getAll =()=>acc.find()
