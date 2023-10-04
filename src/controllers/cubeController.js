cea4204d6af3ca8a1e9596a35c3f164e4d6cd3a8
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-10-04 17:56:15 +0300

    file transfer
    transfer all cube related views to a seperate folder

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -2,7 +2,7 @@ const router = require("express").Router()
 const cubeService = require("../services/cubeService")
 router.get('/create', (req, res) => {
   
-   res.render("create")
+   res.render("cube/create")
 })
 router.post("/create", async (req, res) => {
    const { name, description, imageUrl, difficultyLevel } = req.body
@@ -17,7 +17,7 @@ router.get('/:cubeId/details',async (req, res) => {
       res.redirect("404")
       return
    }
-   res.render('details', { cube })
+   res.render('cube/details', { cube })
 
 
 })
