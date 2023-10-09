c14ddc1c95e34c4b6befb8b083e83f075e94d929
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-10-09 20:46:01 +0300

    user
    Made a model for User entity

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,11 @@ const router=require("express").Router()
 router.get('/register',(req,res)=>{
     res.render('user/register')
 })
+router.post('/register',(req,res)=>{
+    console.log({inputData:req.body})
+    const{username,password,repeatPassword}=req.body;
+    res.redirect('/users/login')
+})
 
 router.get('/login',(req,res)=>{
     res.render('user/login')
---
+++ src/services/userService.js
@@ -0,0 +1,11 @@
+
+
+
+
+exports.register=(username,password,repeatPassword)=>{
+
+
+
+
+    
+}
