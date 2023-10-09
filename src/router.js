8a5287a85578e85f7394742b00f30598aa8aa5d7
Pavel-Kolev <146889340+Pavel-Kolev@users.noreply.github.com>
2023-10-09 20:26:05 +0300

    userController
    register userController,implement get endpoints for login and register

--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 const router= require("express").Router();
 
+const userController=require('./controllers/userController')
 const accesorieController=require("./controllers/accesoryController")
 const homeController=require("./controllers/homeController")
 const cubeController=require("./controllers/cubeController")
 router.use(homeController)
+router.use("/users",userController)
 router.use("/cubes",cubeController)
 router.use("/accessories",accesorieController)
 router.get("*",(req,res)=>{
